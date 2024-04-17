e97920f8d82042593a0bc025a765f6ddd45fe73e
kezagiselle <kezagiselle776@gmail.com>
2024-04-17 14:01:39 +0200

    adding validation....

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ import connectDB from './db/connectDB.js'
 import ErrorHandler from './middleware/Error.handler.js'
 import swaggerUi from 'swagger-ui-express';
 import swagger from './docs/swagger.json' assert {type:"json"}
-
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const corsOption = {
     allowedHeaders: ["Authorization","Content-Type"],
@@ -19,12 +20,49 @@ const corsOption = {
 
 //middleware
 //const app = express();
+//app.use(notfound)
 app.use(express.json());
 //app.use(cors(corsOption));
 app.use('/tasks', taskRouter)
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger));
 
-//app.use(notfound)
+
+app.post('/users',async (req,res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password,10)
+        console.log(salt)
+        console.log(hashedPassword)
+        const user = { name: req.body.name, password: req.body.password }
+        users.push(user)
+        res.status(201).send()
+    } catch (err){
+        next(err)
+    } 
+});
+
+app.post('/users/login', async(req, res) =>{
+    const user = users.find(user => user.name = req.body.name)
+    if(user == null) {
+        return res.status(400).send("can not find user")
+    }
+    try {
+        if(await bcrypt.compare(req.body.password,user.password)){
+            res.send("Success")
+        } else{
+            res.send("Not Allowed")
+        }
+    } catch(err){
+        next(err)
+    }
+})
+
+// app.post('/login',(req,res) => {
+//     const username = req.body.username
+//     const user = { name: username}
+//     const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET))
+// }))
+
+
 
 const port = process.env.PORT || 4000
 
--- utils/validation.js
+++ utils/validation.js
@@ -2,5 +2,6 @@ import {body} from "express-validator";
 
 const addTaskValidation = [
     body("name", "Task name is required").not().isEmpty(),
+    body("email","Email is required").isEmail()
 ];
 export default addTaskValidation;
